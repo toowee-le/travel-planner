cd3235ea9823493b086b23a561bc4e40ffa86e46
Tuyet Le <toowee.le@gmail.com>
2020-06-30 18:44:11 +0100

    re-setup webpack express environment

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,22 +1,23 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
-    devtool: 'source-map',
-    mode: 'development',
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
+    mode: 'production',
     module: {
         rules: [
             {
-                // Transpiles ES6-8 into ES5
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
