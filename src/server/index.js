5e8f14f0d8fd056888ff41f37dade813575606de
Tuyet Le <toowee.le@gmail.com>
2020-07-15 23:35:46 +0100

    install dotenv to hide api keys

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+require('dotenv').config()
 
 // Dependencies
 app.use(bodyParser.urlencoded({ extended: false }))
