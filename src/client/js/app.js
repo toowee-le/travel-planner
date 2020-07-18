b684d8cb96796f78f39e0bc67b924faac3b64039
Tuyet Le <toowee.le@gmail.com>
2020-07-18 16:22:06 +0100

    add functionality to change color of header on scroll

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -63,3 +63,20 @@ export const postRequest = async (url = '', data = {}) => {
         console.log("Error:", err);
     }
 }
+
+// Update UI
+const updateUI = () => {
+    const header = document.getElementsByTagName('header');
+
+    window.onscroll = () => {
+        let top = window.scrollY;
+        // if (top >= 100) {
+        //     header.classList.add('active')
+        // } else {
+        //     header.classList.remove('active')
+        // }
+        console.log(header);
+    }
+}
+
+updateUI();
