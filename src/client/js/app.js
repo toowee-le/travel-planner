2f74cb85efb7b29c7f2a01e2dd7b3546d3deaead
Tuyet Le <toowee.le@gmail.com>
2020-08-04 23:27:28 +0100

    add modal functionality

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,14 +1,25 @@
 import { geonamesAPI, weatherbitAPI, pixabayAPI, restCountriesAPI } from './api'
 
+/**
+ * Global Variables
+ */
+
+let newTrip = {};
+let tripList = document.querySelector('.trip-list');
+let modal = document.querySelector('.modal');
+
+/**
+ * End Global Variables
+ * Begin Main Functions
+ */
+
 export const handleSubmit = async (event) => {
     event.preventDefault();
-
-    let newTrip = {};
 
     // DOM elements needed
     const form = document.forms['travel-form']['to'].value;
     if (form !== '') {
-
+        modal.classList.add('active');
         const destination = document.getElementById('to').value;
 
         await geonamesAPI(destination)
@@ -23,14 +34,15 @@ export const handleSubmit = async (event) => {
         .then(countryInfo => {
             newTrip.region = countryInfo[0].region;
             newTrip.subRegion = countryInfo[0].subregion;
+            newTrip.languages = countryInfo[0].languages[0].name;
             newTrip.currency = countryInfo[0].currencies[0].code;
             newTrip.timezone = countryInfo[0].timezones[1];
         })
 
         await weatherbitAPI(newTrip.lat, newTrip.lon)
         .then(weatherData => {
-            newTrip.currentMinTemp = weatherData.data[0].min_temp;
-            newTrip.currentMaxTemp = weatherData.data[0].max_temp;
+            newTrip.currentMinTemp = Math.floor(weatherData.data[0].min_temp);
+            newTrip.currentMaxTemp = Math.floor(weatherData.data[0].max_temp);
             newTrip.currentTemp = weatherData.data[0].temp;
             newTrip.description = weatherData.data[0].weather.description;
             newTrip.icon = weatherData.data[0].weather.icon;
@@ -39,8 +51,11 @@ export const handleSubmit = async (event) => {
         await pixabayAPI(newTrip.city, newTrip.country)
         .then(photo => {
             newTrip.photo = photo.hits[0].webformatURL;
-            console.log(newTrip)
         })
+
+        console.log(newTrip);
+
+        createNewTrip(newTrip);
     } else {
         alert("Please enter a destination");
     }
@@ -64,19 +79,52 @@ export const postRequest = async (url = '', data = {}) => {
     }
 }
 
-// Update UI
-const updateUI = () => {
-    const header = document.getElementsByTagName('header');
+// Add new trip entry
+const createNewTrip = (data) => { 
+    let innerHTML = `
+        <div class="trip-content">
+            <div class="trip-selected">
+                <h2 class="trip-city">${data.city}, ${data.country}</h2>
+                <img class="trip-photo" src="${data.photo}" alt="City Photo">
+                <div class="trip-info">
+                    <p>Country: <span class="data">${data.country}</span></p>
+                    <p>Region: <span class="data">${data.region}</span></p>
+                    <p>Sub-region: <span class="data">${data.subRegion}</span></p>
+                    <p>Languages: <span class="data">${data.languages}</span></p>
+                    <p>Currency: <span class="data">${data.currency}</span></p>
+                    <p>Timezone: <span class="data">${data.timezone}</span></p>
+                </div>
+            </div>
+            
+            <div class="weather-info">
+                <h3 class="destination">My trip to: <span  class="highlight">${data.city}, ${data.country}</span></h3>
+                <h3 class="departing">Departing: <span class="highlight">14/08/2020</span></h3>
+                <p class="countdown">Your trip to ${data.city} is <strong>10</strong> days away and you will return to London on 22/08/2020.</p>
+                <div class="btn-group">
+                    <button class="btn save-trip">Save trip</button>
+                    <button class="btn delete-trip">Delete trip</button>
+                </div>
+                <hr>
+                <p>Weather forecast:</p>
+                <div class="weather-forecast">
+                    <div class="weather-current">
+                        <p class="weather-date">Current weather:
+                            <br>02/08/2020
+                        </p>
+                        <img class="weather-icon" src="public/${data.icon}.png" alt="Weather Icon">
+                        <p class="weather-description">${data.description}</p>
+                        <p class="weather-temp">Low: ${data.currentMinTemp}&#8451; <span class="divider">|</span> High: ${data.currentMaxTemp}&#8451;</p>
+                    </div>
+                    <div class="weather-future">
+                        <p class="weather-date">Future forecast:<br>14/08/2020</p>
+                        <img class="weather-icon" src="assets/${data.icon}.png" alt="Weather Icon">
+                        <p class="weather-description">Broken Clouds</p>
+                        <p class="weather-temp">Low: 22&#8451; <span class="divider">|</span> High: 25&#8451;</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
 
-    window.onscroll = () => {
-        let top = window.scrollY;
-        // if (top >= 100) {
-        //     header.classList.add('active')
-        // } else {
-        //     header.classList.remove('active')
-        // }
-        console.log(header);
-    }
+    modal.innerHTML = innerHTML;
 }
-
-updateUI();
