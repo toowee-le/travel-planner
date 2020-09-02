3b95ea3bd92411c1347905a70bc2a0e4704440a9
Tuyet Le <toowee.le@gmail.com>
2020-09-02 11:30:48 +0100

    update test to pass failing test

--- src/client/js/helpers.test.js
+++ src/client/js/helpers.test.js
@@ -1,7 +1,7 @@
 import { getDaysLeft, reformatDate } from "./helpers";
 
 test("Return the length of days between current date and departure", () => {
-  expect(getDaysLeft("2020-09-04", Date.now())).toBe(4);
+  expect(getDaysLeft("2020-09-04", "2020-09-02")).toBe(2);
 });
 
 test("Format the date to dd/mm/yyyy", () => {
