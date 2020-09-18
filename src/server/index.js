6e9d48c7ef4dc674dfb0b93105f9f79fd8048603
Tuyet Le <toowee.le@gmail.com>
2020-09-18 12:13:40 +0100

    update sendfile

--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.static("dist"));
 
 app.get("/", function (req, res) {
-  res.sendFile("index.html", { root: path.join(__dirname, "../dist") });
+  res.sendFile("dist/index.html");
 });
 
 // Create empty object to save API data
